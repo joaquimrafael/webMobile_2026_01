c3fa416305c1fc293ce29eac9fe546c42cbe23e5
ggarabs <ggarabetti0607@gmail.com>
2026-03-28 17:48:02 -0300

    feat: add contact us section and clear js functions

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-function carrouselLeftClick() {
+function saveToLocalStorage(newData) {
+  try {
+    const currentData = JSON.parse(localStorage.getItem("messages")) || [];
+    currentData.push(newData);
+    localStorage.setItem("messages", JSON.stringify(currentData));
+  } catch (err) {
+    console.error("Erro ao salvar no localStorage: ", err);
+  }
+}
+
+function moveCarousel(direction) {
   const imagesList = document.getElementsByClassName("carrousel-list")[0];
 
   const elements = imagesList.getElementsByTagName("li");
 
-  let currIndex = Array.from(elements).findIndex((el) => el.id == "visible");
-
+  let currIndex = Array.from(elements).findIndex((el) => el.id === "visible");
   elements[currIndex].removeAttribute("id");
-  elements[
-    (((currIndex - 1) % elements.length) + elements.length) % elements.length
-  ].id = "visible";
+  elements[(currIndex + direction + elements.length) % elements.length].id =
+    "visible";
+}
+
+function carrouselLeftClick() {
+  moveCarousel(-1);
 }
 
 function carrouselRightClick() {
-  const imagesList = document.getElementsByClassName("carrousel-list")[0];
+  moveCarousel(1);
+}
 
-  const elements = imagesList.getElementsByTagName("li");
+function sendMessage(event) {
+  event.preventDefault();
 
-  let currIndex = Array.from(elements).findIndex((el) => el.id == "visible");
-  elements[currIndex].removeAttribute("id");
-  elements[(currIndex + 1) % elements.length].id = "visible";
+  const form = document.getElementById("contact-form");
+
+  const fullName = form.querySelector("#full-name").value.trim();
+  const email = form.querySelector("#email").value.trim();
+  const phone = form.querySelector("#phone").value.trim();
+  const message = form.querySelector("#message").value.trim();
+
+  const newData = {
+    name: fullName,
+    email: email,
+    phone: phone,
+    message: message,
+  };
+
+  saveToLocalStorage(newData);
+
+  window.alert(
+    `Obrigado pela mensagem, ${fullName}! Em breve, entraremos em contato por e-mail ou telefone!`,
+  );
+
+  form.reset();
 }
