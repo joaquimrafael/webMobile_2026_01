13ce5734904c4c20e628879216f357f2d62e7609
ggarabs <ggarabetti0607@gmail.com>
2026-04-04 01:27:15 -0300

    feat: add video in hero and invert the navbar layout

--- script.js
+++ script.js
@@ -52,3 +52,18 @@ function sendMessage(event) {
 
   form.reset();
 }
+
+function toggleNavbar() {
+  const header = document.getElementsByTagName("header")[0];
+  const currState = header.id;
+
+  const body = document.getElementsByTagName("body")[0];
+
+  if (currState == "header-closed") {
+    body.style.gridTemplateColumns = "9rem 1fr";
+    header.id = "header-opened";
+  } else {
+    body.style.removeProperty("grid-template-columns");
+    header.id = "header-closed";
+  }
+}
